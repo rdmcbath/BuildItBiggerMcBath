ca9d3830ac36373e1198d52f585b737f99159858
rdmcbath <rdmcbath@outlook.com>
2017-07-29 17:29:09 -0400

    Backend not launching

--- app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/EndpointAsyncTask.java
@@ -31,7 +31,7 @@ public class EndpointAsyncTask extends AsyncTask<Void, Void, String> {
                         // options for running against local devappserver
                         // - 10.0.2.2 is localhost's IP address in Android emulator
                         // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setRootUrl("http://192.168.1.65:8080/_ah/api/")
                         .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                             @Override
                             public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
@@ -51,7 +51,6 @@ public class EndpointAsyncTask extends AsyncTask<Void, Void, String> {
                 return e.getMessage();
             }
         }
-
 
         @Override
         protected void onPostExecute(String joke) {
